7f6c5b0e21f3b6dd76272e69ba36008f2f4c35eb
Vinicius Feitosa da Silva <oviniciusfeitosa@gmail.com>
2023-09-25 12:44:35 -0300

    ✨ Create Popup file

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,20 +1,44 @@
-import { useState } from 'react'
-import './Popup.css'
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [crx, setCrx] = useState('create-chrome-ext')
+declare const chrome: any;
+
+const Popup = () => {
+  const [selectedTime, setSelectedTime] = useState("30");
+
+  useEffect(() => {
+    chrome.storage.sync.get("notificationInterval", (data: any) => {
+      if (data.notificationInterval) {
+        setSelectedTime(data.notificationInterval);
+      }
+    });
+  }, []);
+
+  const handleTimeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedTime(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const minutes = parseInt(selectedTime);
+    // chrome.alarms.create({ delayInMinutes: 1 });
+    chrome.alarms.create({ delayInMinutes: minutes });
+    chrome.storage.sync.set({ notificationInterval: selectedTime });
+  };
 
   return (
-    <main>
-      <h3>Popup Page!</h3>
+    <div>
+      <h1>Drink Water Reminder</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="time">Select reminder interval:</label>
+        <select id="time" name="time" value={selectedTime} onChange={handleTimeChange}>
+          <option value="30">30 minutes</option>
+          <option value="45">45 minutes</option>
+          <option value="60">60 minutes</option>
+        </select>
+        <button type="submit">Set Reminder</button>
+      </form>
+    </div>
+  );
+};
 
-      <h6>v 0.0.0</h6>
-
-      <a href="https://www.npmjs.com/package/create-chrome-ext" target="_blank">
-        Power by {crx}
-      </a>
-    </main>
-  )
-}
-
-export default App
+export default Popup;
