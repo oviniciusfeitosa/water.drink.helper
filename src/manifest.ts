98dc1d6185bc3364b3ffc6515ce0066d938cdc9e
Vinicius Feitosa da Silva <oviniciusfeitosa@gmail.com>
2023-09-25 12:43:58 -0300

    🔧 Update configs

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,24 +1,24 @@
 import { defineManifest } from '@crxjs/vite-plugin'
 
 export default defineManifest({
-  name: 'create-chrome-ext',
-  description: '',
-  version: '0.0.0',
+  name: 'Drink Water Reminder',
+  description: 'A Chrome extension that reminds you to drink water',
+  version: '1.0.0',
   manifest_version: 3,
   icons: {
-    '16': 'img/logo-16.png',
-    '32': 'img/logo-34.png',
-    '48': 'img/logo-48.png',
-    '128': 'img/logo-128.png',
+    // '16': 'img/logo-16.png',
+    // '32': 'img/logo-34.png',
+    '48': 'img/water-48.png',
+    '128': 'img/water-128.png',
   },
   action: {
     default_popup: 'popup.html',
-    default_icon: 'img/logo-48.png',
+    default_icon: 'img/water-48.png',
   },
-  options_page: 'options.html',
+  // options_page: 'options.html',
   background: {
     service_worker: 'src/background/index.ts',
-    type: 'module',
+    // type: 'module',
   },
   content_scripts: [
     {
@@ -28,9 +28,14 @@ export default defineManifest({
   ],
   web_accessible_resources: [
     {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      resources: [
+        //'img/logo-16.png',
+        //'img/logo-34.png',
+        'img/water-48.png',
+        'img/water-128.png',
+      ],
       matches: [],
     },
   ],
-  permissions: [],
+  permissions: ['alarms', 'notifications', 'storage'],
 })
