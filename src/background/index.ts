08da6690f86cd82d157a256752627bade3a1799c
Vinicius Feitosa da Silva <oviniciusfeitosa@gmail.com>
2023-09-25 12:44:12 -0300

    ✨ Set alarm listener

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,16 @@
-console.info('chrome-ext template-react-ts background script')
+// console.info('chrome-ext template-react-ts background script')
 
-export {}
+// export {}
+
+declare const chrome: any;
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.alarms.onAlarm.addListener((alarm: any) => {
+    chrome.notifications.create({
+      type: "basic",
+      iconUrl: "img/water-128.png",
+      title: "Drink Water Reminder",
+      message: "It's time to drink water!",
+    });
+  });
+});
